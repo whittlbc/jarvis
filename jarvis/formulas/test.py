0f643f5124ca781bc614e33fc681661ee6aa325a
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-09-29 23:47:29 -0500

    simplifying test formula

--- jarvis/formulas/test.py
+++ jarvis/formulas/test.py
@@ -3,4 +3,4 @@ from jarvis import jarvis
 
 @jarvis.listen('test')
 def time(event):
-	jarvis.respond('Test Heard. Here are your matches: {}'.format(event.matches))
+	jarvis.respond('Heard testing')
