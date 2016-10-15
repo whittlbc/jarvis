24ec2ce1a845897e9c2635877269ecca29cef702
Ben Whittle <benWhittle@rescomp-16-331629.stanford.edu>
2016-10-15 01:53:19 -0700

    added soundbite functionality

--- jarvis/actions/random.py
+++ jarvis/actions/random.py
@@ -2,5 +2,10 @@ from jarvis.core.responder import respond
 
 
 def yeezy(e):
-	# respond({'audio': 'http://confluxapp.s3-website-us-west-1.amazonaws.com/files/ytm.m4a'})
-	respond('Yeezy taught me.', with_audio=False)
+	respond(
+		'Yeezy taught me.',
+		with_audio=True,
+		data={
+			'soundbite': 'http://confluxapp.s3-website-us-west-1.amazonaws.com/files/ytm.m4a'
+		}
+	)
