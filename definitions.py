0d07a7379bd5c8c5fc38e42b6ecd5fb0497d89aa
Ben Whittle <benWhittle@Bens-MacBook-Pro.local>
2016-11-01 11:46:03 -0700

    testing out a working example of a seq2seq from DeepQA repo -- great example

--- definitions.py
+++ definitions.py
@@ -1,10 +1,9 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 formulas_dir = basedir + '/jarvis/formulas'
 classifier_model_path = basedir + '/jarvis/learn/classify/model.pkl'
 conversational_model_path = basedir + '/jarvis/learn/converse/model.pkl'
 actions_data_path = basedir + '/data/actions'
-actions_data_path = basedir + '/data/converse'
-default_speech_file = basedir + '/tmp/speech.ogg'
-s3_speech_path = 'jarvis/speech.ogg'
+converse_data_path = basedir + '/data/converse'
---
+++ learn.py
@@ -0,0 +1,5 @@
+from jarvis.learn.converse.trainer import Trainer
+
+
+if __name__ == "__main__":
+	Trainer().main()
