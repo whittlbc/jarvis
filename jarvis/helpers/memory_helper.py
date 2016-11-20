82f9e778b49b9e785875cf09c3e2ef7cd1705c82
Ben Whittle <benwhittle31@gmail.com>
2016-11-20 13:39:58 -0500

    formatting predicate content into modeled content

--- jarvis/helpers/memory_helper.py
+++ jarvis/helpers/memory_helper.py
@@ -35,13 +35,123 @@ def format_memory(text):
 	if not chopped_subject:
 		return None, None
 	
-	format = get_predicate_format(pred_tree)
+	format, pred_content = get_predicate_format(pred_tree)
 	
 	if format not in PREDICATE_FORMATS:
 		return None, None
 	
+	leading_v = pred_content[0]
+	modeled_content = format_modeled_content(pred_content[1:])
+	
+	# pred_handler = {
+	# 	'V(BE)': handle_be,
+	# 	'V(OWN)': handle_own,
+	# 	'V*': handle_action
+	# 	# ignore MD starter for now...won't have to do this if we strip those anyways.
+	# }[format[0]]
+	
+	# info = pred_handler(pred_content)
+	
+	
+	# if predicate has a noun phrase in it, we're dealing with some sort of relationship
+	# if format_has_label(format, phrases.NOUN_PHRASE):
+		# Figure out what type of relationship
+		# print
+	
+	# Do what you need to with chopped_subject
+	# both choppped_subject['noun'] and chopped_subject['owner'] are subjects (2 upsertions)
+	# if chopped_subject['owner'] exists, then you know you have a relationship of S_POSS_S to upsert
+	# if either choppped_subject['description']['adj'] or choppped_subject['description']['adv'] have contents, then you
+	# know you'll need to upsert a description (if no possesssion, upsert a SubjectDescription for noun; if possession,
+	# upsert a RelationshipDescription where 'component' is noun)
+	
+	
 	# Need to send back info about what was saved so that Jarvis can reiterate that to the user
 	return chopped_subject, format
+
+
+# [
+# 	[{'V*': 'playing'}, {'NP': {'owner': None, 'noun': 'basketball', 'description': {'adv': [], 'adj': []}}}]
+# ]
+def format_modeled_content(data):
+	modeled_data = []
+	content = {}
+		
+	for child in data:
+		if isinstance(child, list):
+			modeled_data.extend([format_modeled_content(child)])
+			return modeled_data
+		else:
+			key = child.keys()[0]
+			val = child[key]
+			
+			if key == 'V*':
+				content['action'] = {
+					'v': val,
+					'adj': [],
+					'adv': [],
+					'subject': None
+				}
+				
+			elif key == 'NP':
+				if content.get('action'):
+					content['action']['subject'] = val
+				else:
+					content['subject'] = val
+			
+	modeled_data.append(content)
+			
+	return modeled_data
+
+
+def handle_be(pred_content):
+	content = []
+	
+	for child in pred_content[1:]:
+		label = child.keys()[0]
+		
+		data = {}
+		
+		if label == phrases.VERB_PHRASE:
+			content.extend([handle_be(child[label])])
+		
+		elif label == phrases.NOUN_PHRASE:
+			data['noun'] = chop_np(child[label])
+			content.append(data)
+			
+		elif label == phrases.PREP_PHRASE:
+			data['pp'] = chop_pp(child[label])
+			content.append(data)
+		
+		elif label in verbs:
+			data['action'] = child[label]
+			content.append(data)
+		
+	return content
+
+
+def handle_own(pred_content):
+	# NP exists. You know this already
+	print
+
+
+def handle_action(pred_content):
+	print
+	
+
+def format_has_label(format, label, final_return=True):
+	bools = []
+	
+	for c in format:
+		if isinstance(c, dict):
+			bools.extend(format_has_label(c[c.keys()[0]], label, final_return=False))
+		else:
+			bools.append(c == label)
+
+	if final_return:
+		return True in bools
+	else:
+		return bools
 
 
 def valid_top_level_structure(t):
@@ -69,6 +179,11 @@ def labeled_leaves(tree):
 			leaves.append([tree.label(), child])
 	
 	return leaves
+
+
+# Need to format a response into some when/where/with style structure
+def chop_pp(pp):
+	return {}
 
 
 def chop_np(np):
@@ -100,6 +215,9 @@ def chop_np(np):
 			
 		i += 1
 	
+	if not subject['noun']:
+		return None
+	
 	data = {
 		'noun': subject['noun'],
 		'owner': None,
@@ -115,14 +233,17 @@ def chop_np(np):
 		
 	return data
 
+
 def corrected_owner(owner):
 	if owner.lower() in ['my', 'our']:
 		return 'I'
 	else:
 		return owner
 	
+	
 def get_predicate_format(tree):
 	format = []
+	format_content = []
 	
 	for child in tree:
 		if is_tree(child):
@@ -132,9 +253,11 @@ def get_predicate_format(tree):
 				pass
 			
 			elif label in [clauses.DECLARATION, phrases.VERB_PHRASE]:
-				d = {}
-				d[label] = get_predicate_format(child)
-				format.extend([d])
+				f = {}
+				f[label], fc = get_predicate_format(child)
+				
+				format.extend([f])
+				format_content.extend([fc])
 				
 			elif label in [phrases.NOUN_PHRASE, phrases.PREP_PHRASE]:
 				pp_children = [c for c in child if has_label(c, phrases.PREP_PHRASE)]
@@ -144,16 +267,32 @@ def get_predicate_format(tree):
 				
 				format.append(label)
 				
+				content = {}
+				
+				if label == phrases.NOUN_PHRASE:
+					content[label] = chop_np(child)
+				else:
+					content[label] = chop_pp(child)
+				
+				format_content.append(content)
+				
 			elif label in verbs:
-				format.append(get_verb_tag(child[0]))
+				corrected_verb_tag = get_verb_tag(child[0])
+				format.append(corrected_verb_tag)
+				content = {}
+				content[corrected_verb_tag] = child[0]
+				format_content.append(content)
 				
 			elif label in [words.MODAL, words.TO]:
 				format.append(label)
+				content = {}
+				content[label] = child[0]
+				format_content.append(content)
 				
 			else:
 				error('Invalid tree label while parsing predicate: {}'.format(label))
 
-	return format
+	return format, format_content
 				
 
 def get_verb_tag(verb):
