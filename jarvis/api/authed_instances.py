290baaa834100ab5f7a977992582ebb1ca4aebc2
Ben Whittle <benwhittle31@gmail.com>
2017-02-23 18:19:52 -0800

    started on getting an authed instance of uber and modified attentionPrompt regex

--- jarvis/api/authed_instances.py
+++ jarvis/api/authed_instances.py
@@ -1,6 +1,6 @@
 from jarvis.helpers.configs import config
 
- 
+
 # def reddit(configs):
 # 	import praw
 #
@@ -20,10 +20,25 @@ def weather():
 	return pyowm.OWM(config('OWM_API_KEY'))
 
 
-def uber(access_token=None):
-	# TODO: This.
-	import uber_rides
-	return
+def uber(access_token=None, refresh_token=None):
+	from uber_rides.client import UberRidesClient
+	from uber_rides.session import OAuth2Credential
+	from uber_rides.session import Session
+	
+	oauth2credential = OAuth2Credential(
+		client_id=config('UBER_CLIENT_ID'),
+		access_token=access_token,
+		# Not sure about these 4:
+		# expires_in_seconds=credentials.get('expires_in_seconds'),
+		# scopes=credentials.get('scopes'),
+		# grant_type=credentials.get('grant_type'),
+		# redirect_url=credentials.get('redirect_url'),
+		client_secret=config('UBER_CLIENT_SECRET'),
+		refresh_token=refresh_token
+	)
+	
+	session = Session(oauth2credential=oauth2credential)
+	return UberRidesClient(session, sandbox_mode=(not config('PRODUCTION')))
 
 
 def places():
